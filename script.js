ae68dfe3c788a57b8d7d9bd45ea2cebd7254e7f0
joao <88242934+jvsilva1998@users.noreply.github.com>
2025-03-11 16:12:04 -0400

    responsivo
    responsivo

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const sections = document.querySelectorAll('section');
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("section");
 
-    sections.forEach(section => {
-        section.classList.add('fade-in');
-    });
+  sections.forEach((section) => {
+    section.classList.add("fade-in");
+  });
 
-    const form = document.getElementById('contact-form');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        alert('Mensagem enviada com sucesso!');
-        form.reset();
-    });
+  const form = document.getElementById("contact-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Mensagem enviada com sucesso!");
+    form.reset();
+  });
+
+  // script.js
+  document.getElementById("mobile-menu").addEventListener("click", function () {
+    const navList = document.getElementById("nav-list");
+    navList.classList.toggle("active"); // Alterna a classe "active" para mostrar ou esconder o menu
+  });
 });
